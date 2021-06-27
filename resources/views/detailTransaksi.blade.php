1aed8882d74a8c33b76bdcfcda32a3043c13cb17
okakakikioAxe <lukmanmuhamadilham@gmail.com>
2021-06-28 02:14:16 +0700

    tambah detail transaksi

---
+++ resources/views/detailTransaksi.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.admin')
+
+@section('title','admin | transaksi')
+
+@section('konten')
+<div class="container">
+    <h3 class="text-center">DETAIL TRANSAKSI</h3>
+</div>
+<br>
+
+<div class="container">
+    
+</div>
+
+@endsection
